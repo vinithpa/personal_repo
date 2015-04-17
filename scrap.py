82a9204f2c7f3a1248c89d97a0a05f94ef91aa10
Vinith Annam <vinith@equateanalytics.com>
2015-04-16 22:25:03 -0400

    Crunch project and any scrap code

---
+++ scrap.py
@@ -0,0 +1,12 @@
+# class CampaignComparison(object):
+
+# 	def __init__(self,campaign_id1,campaign_id2):
+
+# 		self.campaign_id1 = campaign_id1
+# 		self.campaign_id2 = campaign_id2
+
+# 		self.id1_output = executeEquateQuery(campaign_id1.query_selection)
+# 		self.id2_output = executeEquateQuery(campaign_id2.query_selection)
+
+# 		print tuple(self.id1_output)[0][0]
+# 		print tuple(self.id2_output)[0][0]
